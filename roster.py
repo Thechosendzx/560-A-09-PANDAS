aa236385f8197a224c61ad121db8378ca367ac1f
ryandeng <158107814+Thechosendzx@users.noreply.github.com>
2024-04-06 23:39:54 -0400

    Add files via upload

--- roster.py
+++ roster.py
@@ -1,5 +1,14 @@
 #https://goheels.com/sports/mens-basketball/roster
+import pandas as pd
 
-roster=[ "bacot","davis","cadeau"]
-for player in roster:
-    print(roster)
+
+player = {"last name":["bacot","davis","cadeau"],
+          "first name": ["armando","rj","elliot"],
+          "height":[83,72,73],
+          "weight":[240,180,180]}
+data= pd.DataFrame(player)
+
+data["bmi"]=(data["weight"]/2.205)/((data["height"]/39.7)**2)
+print(data)
+
+data.to_csv("bmi.csv")
